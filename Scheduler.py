aef2b1f5236f0e35ca9d874e8f9b9149c2b207f6
fanghejin <fanghejin@qq.com>
2024-10-30 22:28:34 +0800

    写了非必要不下载的逻辑

--- Scheduler.py
+++ Scheduler.py
@@ -18,37 +18,64 @@ class Scheduler:
 
     def run(self):
         downloader = Downloader()
-        parser = Parser()
+        if os.listdir("originPages").count("additivePage.html") == 0:
+            print("下载添加剂表成功")
+            additivePage = downloader.Download(ROOT_ADDITIVE)
+            with open("./originPages/additivePage.html", "w") as f:
+                f.write(additivePage)
 
-        addUrlList = parser.ParseAdditiveUrls(downloader.Download(ROOT_ADDITIVE))
-        print("下载添加剂表成功")
+        parser = Parser()
+        with open("additivePage.html", "r") as f:
+            addUrlList = parser.ParseAdditiveUrls(f.read())
+
         print("添加剂表处理完成")
         self.additiveTb = pd.DataFrame(
             columns=["INS号", "CNS号", "中文名称","英文名称","功能","质量规格标准","JECFA规格资料","备注"],
-            index=addUrlList,
+            index=[i[0] for i in addUrlList],
         )
-        # self.additiveTb.loc[:,"url"]=addUrlList
 
+        # 下载所需要的页面
+        fileNames = os.listdir("originPages")
+        if len(fileNames)==0:
+            for i,(addUrl,addName) in enumerate(addUrlList):
+                print(f"{i}.下载{addName}的资料")
+                page = downloader.Download(addUrl)
+                print(f"{i}.{addName}的资料下载成功")
+                with open(f"./originPages/{i}.html", "w") as f:
+                    f.write(page)
+
+            downloader.driver.quit()
+
+        # 筛选信息
+        failAdds=[]
         for i,(addUrl,addName) in enumerate(addUrlList):
-            print(f"{i}.下载{addName}的资料")
-            page = downloader.Download(addUrl)
-            print(f"{i}.{addName}的资料下载成功")
-            with open(f"./originPages/{i}.html", "w") as f:
-                f.write(page)
+            with open(f"./originPages/{i}.html", "r") as f:
+                page = f.read()
+                try:
+                    addRow,addRel = parser.ParseAdditiveAttrs(page)
+                    for key,value in addRow.items():
+                        self.additiveTb.loc[addUrl, key]=value
 
-            # addRow,addRel = parser.ParseAdditiveAttrs(page)
-            # for key,value in addRow.items():
-            #     self.additiveTb.loc[addUrl, key]=value
+                    addFoodTb=pd.DataFrame(data=addRel)
+                    addFoodTb.columns=["食品分类号","食品名称","最大使用量（g/kg）","备注"]
+                    addFoodTb.to_excel(f"./food/{addName[:30]}.xlsx")
+                    print(f"添加剂{addName}处理完成")
 
-            # addFoodTb=pd.DataFrame(data=addRel)
-            # addFoodTb.columns=["食品分类号","食品名称","最大使用量（g/kg）","备注"]
-            # addFoodTb.to_excel(f"./food/{addName[:30]}.xlsx")
-            # print(f"添加剂{addName}处理完成")
+                except Exception as e:
+                    print(e)
+                    print(f"{addName}({addUrl})信息处理失败")
+                    failAdds.append(f"{addName}({addUrl})")
 
 
-        # self.additiveTb.to_excel("AdditiveTable.xlsx")
-        # self.foodTb.to_excel("FoodTable.xlsx")
-        downloader.driver.quit()
+        self.additiveTb.to_excel("AdditiveTable.xlsx")
+        self.foodTb.to_excel("FoodTable.xlsx")
+
+        print(f"处理完毕，{len(failAdds)}个处理失败：")
+        with open("failed.txt", "w") as f:
+            f.writelines(failAdds)
+
+        print("处理失败的添加剂信息写入到failed.txt中")
+
 
 
 if __name__ == "__main__":
